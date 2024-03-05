5b56785ec6c3c29635610fa779a45b8000b53dae
Ларьков Алексей Юрьевич <lau@isc.by>
2024-03-05 16:08:21 +0300

    Доработана программа в рамках ввода текста и цвета текста

--- 8m.py
+++ 8m.py
@@ -4,14 +4,14 @@ from PIL import Image, ImageTk
 
 password = " "
 question = " "
-
+name = " "
 # Создание последнего окна
 def new_window():
     window = tk.Tk()
     window.title("Приглашение")
     window.attributes("-topmost", True)
         
-    info = tk.Label(window, text= "Приглашаем Вас сюда на банкет в 12:00!!", font=("Verdana",20), fg="red")
+    info = tk.Label(window, text= "Приглашаем Вас сюда для открытия \n этого замечательного праздника \n на 1 этаж в 12:00!!", font=("Verdana",20), fg="PaleVioletRed3")
     info.pack()
         
     image = Image.open('//10.50.100.25/isc/Для ОБМЕНА/test_copy/3s.jpg')
@@ -20,7 +20,7 @@ def new_window():
     label_1.image = photo_sec
     label_1.pack()
         
-    but = tk.Button(window, text="Закрыть окно", command=lambda: window.destroy(), bg="red", fg="black")
+    but = tk.Button(window, text="Закрыть окно", command=lambda: window.destroy(), bg="PaleVioletRed3", fg="black")
     but.pack()
 
 #чтобы нельзя было закрыть окно
@@ -37,7 +37,11 @@ def sec_window():
     root.attributes("-topmost", True)
     root.attributes("-toolwindow", True)
     root.protocol("WM_DELETE_WINDOW", not_closing)
-        
+    
+    #Создаем приветствие
+    info_1 = tk.Label(root, text="С 8 марта, {}".format(name), font=("Verdana",20), fg="PaleVioletRed3", anchor="n")
+    info_1.pack()    
+    
     # Загружаем изображение
     image = Image.open('//10.50.100.25/isc/Для ОБМЕНА/test_copy/2s.jpg')
     photo = ImageTk.PhotoImage(image)
@@ -46,9 +50,9 @@ def sec_window():
     label = tk.Label(root, image=photo)
     label.image = photo
     label.pack()
-    
+        
     # Создаем вопрос
-    info = tk.Label(root, text= question, font=("Verdana",20), fg="red")
+    info = tk.Label(root, text="Отгадайте загадку и введите ответ.\n {}".format(question), font=("Verdana",20), fg="PaleVioletRed3")
     info.pack()
         
     # Создаем поле для ввода кода
@@ -57,14 +61,17 @@ def sec_window():
     
     # Функция для проверки введенного кода
     def check_code():
-        if entry.get() == password:
+        passw = entry.get()
+        passw = passw.strip()
+        passw = passw.lower()
+        if passw == password:
             root.destroy()
             new_window()
         else:
             messagebox.showinfo("Неверный код", "А вот и нет, нужно ввести код еще раз")
 
     # Создаем кнопку для ввода кода
-    button = tk.Button(root, text="Ввести код", command=check_code, bg="red", fg="black")
+    button = tk.Button(root, text="Ввести ответ", command=check_code, bg="PaleVioletRed3", fg="black")
     button.pack()
   
 # Создаем главное окно
@@ -81,7 +88,7 @@ photo = ImageTk.PhotoImage(file= '//10.50.100.25/isc/Для ОБМЕНА/test_co
 label = tk.Label(main, image=photo)
 label.pack()
 
-info = tk.Label(main, text= "Введите вашу фамилию с большой буквы", font=("Verdana",20), fg="red")
+info = tk.Label(main, text= "Введите вашу фамилию", font=("Verdana",20), fg="PaleVioletRed3")
 info.pack()
         
 # Создаем поле для ввода кода
@@ -92,68 +99,81 @@ entry.pack()
 def check_surname():
     global question
     global password
+    global name
     surname = entry.get()
     surname = surname.strip()
-    if surname == "Павлович":
-        password = "1"
-        question = "Первая цифра"
+    surname = surname.lower()
+    if surname == "павлович":
+        password = "календарь"
+        question = "На раскрашенных страницах много праздников хранится"
+        name = "Анна Павловна"
         main.destroy()
         sec_window()
-    if surname == "Литвиненко":
-        password = "2"
-        question = "Вторая цифра"
+    elif surname == "литвиненко":
+        password = "подснежник"
+        question = "Белый, синий, голубой. \n Появился он весной."
+        name = "Анастасия Александровна"
         main.destroy()
         sec_window()
-    if surname == "Ларьков":
-        password = "Спасибо"
+    elif surname == "ларьков":
+        password = "спасибо"
         question = "Волшебное слово?"
+        name = "Жулик"
         main.destroy()
         sec_window()
-    if surname == "Малинская":
-        password = "3"
-        question = "Третья?"
+    elif surname == "малинская":
+        password = "порядка"
+        question = "Чего нет в женской сумочке?"
+        name = "Анастасия Николаевна"
         main.destroy()
         sec_window()
-    if surname == "Шутова":
-        password = "4"
-        question = "Четвертая?"
+    elif surname == "шутова":
+        password = "конфеты"
+        question = "Что мужчины всей планеты \n Дарят женщинам?..."
+        name = "Галина Григорьевна"
         main.destroy()
         sec_window()
-    if surname == "Александрович":
-        password = "5"
-        question = "Пятая?"
+    elif surname == "александрович":
+        password = "февраль"
+        question = "В каком месяце болтушка Валя говорит меньше всего?"
+        name = "Анна Викторовна"
         main.destroy()
         sec_window()
-    if surname == "Юзефович":
-        password = "6"
-        question = "Шестая?"
+    elif surname == "юзефович":
+        password = "подснежник"
+        question = "Белый, синий, голубой. \n Появился он весной."
+        name = "Елена Валерьевна"
         main.destroy()
         sec_window()
-    if surname == "Кочубей":
-        password = "7"
-        question = "Седьмая?"
+    elif surname == "кочубей":
+        password = "конфеты"
+        question = "Что мужчины всей планеты \n Дарят женщинам?..."
+        name = "Ксения Владимировна"
         main.destroy()
         sec_window()
-    if surname == "Заяц":
-        password = "8"
-        question = "Восьмая?"
+    elif surname == "заяц":
+        password = "порядка"
+        question = "Чего нет в женской сумочке?"
+        name = "Татьяна Владимировна"
         main.destroy()
         sec_window()
-    if surname == "Дмитренок":
-        password = "9"
-        question = "Девятая?"
+    elif surname == "дмитренок":
+        password = "февраль"
+        question = "В каком месяце болтушка Валя говорит меньше всего?"
+        name = "Валентина Ивановна"
         main.destroy()
         sec_window()
-    if surname == "Веремейчик":
-        password = "10"
-        question = "Десятая?"
+    elif surname == "веремейчик":
+        password = "календарь"
+        question = "На раскрашенных страницах много праздников хранится"
+        name = "Ольга Геннадьевна"
         main.destroy()
         sec_window()
     else:
         messagebox.showinfo("Неправильная фамилия", "Проверьте правильность фамилии! Попробуйте ещё раз!!!")
 
 # Создаем кнопку для ввода кода
-button = tk.Button(main, text="Ввести фамилию", command=check_surname, bg="red", fg="black")
+button = tk.Button(main, text="Ввести фамилию", command=check_surname, bg="PaleVioletRed3", fg="black")
 button.pack()
      
 # Запускаем главный цикл обработки событий
